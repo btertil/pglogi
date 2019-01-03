5a8962c74b702d3b21b4e21a5c1525250212b008
Bartosz Tertil <bartosz.tertil@gmail.com>
2019-01-03 17:04:17 +0100

    first commit

---
+++ analiza.sql
@@ -0,0 +1,172 @@
+
+
+-- najlepsze
+select * from dl_models order by test_accuracy desc;
+
+-- analiza
+select batch_size, count(*) ile, avg(test_accuracy) from dl_models group by batch_size order by avg(test_accuracy) desc;
+select lr, count(*) ile, avg(test_accuracy) from dl_models where lr <> 0.1 group by lr order by avg(test_accuracy) desc;
+
+select epochs, count(*) ile, avg(test_accuracy) from dl_models group by epochs order by avg(test_accuracy) desc;
+
+-- to warto tylko jeśłi inne hyperparameters, beta, beta2 itd
+select
+  lr,
+  batch_size,
+  epochs,
+  count(*) ile,
+  avg(test_accuracy)
+from dl_models
+group by
+  lr,
+  batch_size,
+  epochs
+order by 5 desc;
+
+
+-- czas szkolenia modelu
+-- czas wykonywania na podstawie entered w bazie (uwaga, wirtualka, czas był rozjechany w service ntpd restart spowodował przesunięcie w 1 miejscu)
+select
+    m.*,
+    case
+        when python_model_id < 73 then 1
+        when python_model_id >= 73 and python_model_id < 101 then 2
+        when python_model_id >= 101 and python_model_id < 250 then 3
+        when python_model_id >= 250 and python_model_id < 310 then 4
+        when python_model_id >= 250 and python_model_id < 460 then 5
+        when python_model_id >= 460 and python_model_id < 668 then 6
+        when python_model_id >= 668 and python_model_id < 1494 then 7
+        else 8
+    end search_id,
+    entered -  lag(entered, 1) over (partition by
+        case
+            when python_model_id < 73 then 1
+            when python_model_id >= 73 and python_model_id < 101 then 2
+            when python_model_id >= 101 and python_model_id < 250 then 3
+            when python_model_id >= 250 and python_model_id < 310 then 4
+            when python_model_id >= 250 and python_model_id < 460 then 5
+            when python_model_id >= 460 and python_model_id < 668 then 6
+            when python_model_id >= 668 and python_model_id < 1494 then 7
+            else 8
+        end order by id
+    ) time_diff
+from
+    dl_models m
+where 1 = 1
+order by
+    case
+        when python_model_id < 73 then 1
+        when python_model_id >= 73 and python_model_id < 101 then 2
+        when python_model_id >= 101 and python_model_id < 250 then 3
+        when python_model_id >= 250 and python_model_id < 310 then 4
+        when python_model_id >= 250 and python_model_id < 460 then 5
+        when python_model_id >= 460 and python_model_id < 668 then 6
+        when python_model_id >= 668 and python_model_id < 1494 then 7
+        else 8
+    end,
+    entered;
+
+
+-- view v_dl_models_runs
+drop view if exists v_dl_models_runs cascade;
+create or replace view v_dl_models_runs as
+    select
+        m.*,
+        case
+            when python_model_id < 73 then 1
+            when python_model_id >= 73 and python_model_id < 101 then 2
+            when python_model_id >= 101 and python_model_id < 250 then 3
+            when python_model_id >= 250 and python_model_id < 310 then 4
+            when python_model_id >= 250 and python_model_id < 460 then 5
+            when python_model_id >= 460 and python_model_id < 668 then 6
+            when python_model_id >= 668 and python_model_id < 1494 then 7
+            else 8
+        end run_id,
+        entered -  lag(entered, 1) over (partition by
+            case
+                when python_model_id < 73 then 1
+                when python_model_id >= 73 and python_model_id < 101 then 2
+                when python_model_id >= 101 and python_model_id < 250 then 3
+                when python_model_id >= 250 and python_model_id < 310 then 4
+                when python_model_id >= 250 and python_model_id < 460 then 5
+                when python_model_id >= 460 and python_model_id < 668 then 6
+                when python_model_id >= 668 and python_model_id < 1494 then 7
+                else 8
+            end order by id
+        ) time_diff
+    from
+        dl_models m
+    where 1 = 1
+    order by
+        case
+            when python_model_id < 73 then 1
+            when python_model_id >= 73 and python_model_id < 101 then 2
+            when python_model_id >= 101 and python_model_id < 250 then 3
+            when python_model_id >= 250 and python_model_id < 310 then 4
+            when python_model_id >= 250 and python_model_id < 460 then 5
+            when python_model_id >= 460 and python_model_id < 668 then 6
+            when python_model_id >= 668 and python_model_id < 1494 then 7
+            else 8
+        end,
+        entered;
+
+select * from v_dl_models_runs;
+
+-- view v_dl_models_performance
+create or replace view v_dl_models_performance as
+  select * from v_dl_models_runs order by test_accuracy desc;
+
+select * from v_dl_models_performance;
+
+
+-- models per run_id
+select run_id, count(*) ile, avg(test_accuracy) from v_dl_models_runs group by 1 order by avg(test_accuracy) desc;
+
+
+-- speed & updates
+select
+  run_id,
+  count(*) ile,
+  avg(time_diff) avg_time_diff,
+  avg((20090 * epochs) / batch_size) avg_updates,
+  avg(((20090 * epochs) / batch_size) / (extract(epoch from time_diff)))  avg_speed,
+  avg(test_accuracy) avg_test_accuracy
+from v_dl_models_runs
+where id not in (34, 35)
+group by 1
+order by 5 desc;
+
+
+-- best model per run_id
+select
+   id,
+   python_model_id,
+   lr,
+   batch_size,
+   epochs,
+   test_loss,
+   test_accuracy,
+   entered,
+   run_id,
+   time_diff training_time,
+   patience
+from (
+    select
+      r.*,
+      max(test_accuracy) over (partition by run_id) ma
+    from v_dl_models_runs r
+    ) s
+where
+  ma = test_accuracy
+order by
+  test_accuracy desc;
+
+-- updates
+
+update dl_models set patience = 10 where patience is null and python_model_id >= 1494;
+
+delete from dl_models where python_model_id = 640;
+
+select '2 days 2 seconds' :: interval;
+
+select '2 days 2 seconds' :: interval interval_time, extract(epoch from '2 days 2 seconds' :: interval) epoch_time;
---
+++ pglogi_early_stopping.py
@@ -0,0 +1,272 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  2 18:07:31 2019
+
+@author: bondi
+"""
+
+import sys
+from time import sleep
+import numpy as np
+import pandas as pd
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, BatchNormalization, Softmax, Activation, Dropout
+from keras.activations import relu
+from keras.initializers import VarianceScaling
+from keras.metrics import categorical_accuracy
+from keras.utils import to_categorical
+from keras.wrappers import scikit_learn
+from keras.callbacks import EarlyStopping
+
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+
+import psycopg2
+import psycopg2.extras  # cursor
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style("darkgrid")
+# %%
+
+# Try to connect
+try:
+    conn = psycopg2.connect(host='192.168.0.101', user='bartek', password='Aga', database='logs', port=5432)
+    # utf8
+    conn.set_client_encoding('UTF8')
+    # cursor
+    cur = conn.cursor()
+except:
+    print("I am unable to connect to the database.")
+    conn = None
+    cur = None
+    sys.exit(1)
+
+
+# %%
+
+logs_df = pd.read_sql_query('select * from akuratne_25k', conn)
+print(logs_df.shape)
+logs_df.head()
+
+print(logs_df.shape)
+
+features = ['rank_w_ip', 'avg_id_rows_current', 'id_parity', 'rank_w_ip_parity', 'the_same_parity']
+print(features)
+
+target = 'target_1'
+print(target)
+
+X = logs_df[features]
+X.head()
+
+y = logs_df[target]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=324)
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+scaler = StandardScaler().fit(X_train)
+
+# std scaler jest szkolony tylko na train
+# wszelkie przetwarzanie testu na podstawie regół, znalezioych dla train, także dla pre-process
+X_train_scaled = scaler.transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+
+# %%
+def fit_and_evaluate_model(models, model_id=0, lr=0.001, batch_size=1024, epochs=3500, patience=None):
+    # Keras model
+    k_model = Sequential()
+    # 1st layer
+    # k_model.add(Dense(units=512, kernel_initializer=VarianceScaling, input_shape=X.shape[0], activation=None))
+    k_model.add(Dense(128, activation=None, input_shape=(5,)))
+    k_model.add(BatchNormalization())
+    k_model.add(Activation("relu"))
+    k_model.add(Dropout(rate=0.1))
+    # 2nd layer
+    k_model.add(Dense(64, activation=None))
+    k_model.add(BatchNormalization())
+    k_model.add(Activation("relu"))
+    k_model.add(Dropout(rate=0.1))
+    # 3nd layer
+    k_model.add(Dense(16, activation=None))
+    k_model.add(BatchNormalization())
+    k_model.add(Activation("relu"))
+    # 4nd layer
+    k_model.add(Dense(1, activation=None))
+    k_model.add(BatchNormalization())
+    k_model.add(Activation("sigmoid"))
+
+    k_model.compile(loss=keras.losses.binary_crossentropy, optimizer=keras.optimizers.Adam(lr=lr), metrics=["accuracy"])
+
+    # callbacks
+    callbacks = EarlyStopping(monitor='val_loss',
+                              min_delta=0,
+                              patience=patience,
+                              verbose=0, mode='auto')
+
+    # Train
+    history = k_model.fit(X_train_scaled, y=y_train.values, epochs=epochs,
+                          batch_size=batch_size, validation_split=0.25,
+                          callbacks=[callbacks], verbose=0)
+
+    # Plot training history
+    def plot_accuracy_and_loss(trained_model, test_accuracy):
+
+        global conn
+        global cur
+        validation = False
+
+        hist = trained_model.history
+        acc = hist['acc']
+        loss = hist['loss']
+        try:
+            val_acc = hist['val_acc']
+            val_loss = hist['val_loss']
+            validation = True
+        except KeyError as e:
+            print("No validation data defined, showing only training set hostory")
+        epochsw = range(1, len(acc) + 1)
+
+        fig, ax = plt.subplots(1, 2, figsize=(14, 6))
+
+        plt.suptitle("Model_id={0}: lr={1}, batch_size={2}, epochs={3}, test_accuracy={4: .10f}" \
+                     .format(model_id, lr, batch_size, epochs, test_accuracy), color='grey', fontsize=20)
+
+        ax[0].tick_params(colors="grey")
+        ax[0].plot(epochsw, acc, 'g', label='Training accuracy')
+        if validation:
+            ax[0].plot(epochsw, val_acc, 'r', label='Validation accuracy')
+        ax[0].set_ylabel('Accuracy', color='grey', fontsize=12)
+        ax[0].set_title('Training and validation accuracy', color='grey', fontsize=16)
+        ax[0].set_xlabel('Epochs', color='grey', fontsize=12)
+
+        ax[0].legend()
+        ax[0].grid(True)
+
+        ax[1].tick_params(colors="grey")
+        ax[1].plot(epochsw, loss, 'g', label='Training cost')
+        if validation:
+            ax[1].plot(epochsw, val_loss, 'r', label='Validation cost')
+        ax[1].legend()
+        ax[1].set_title('Training and validation loss', color='grey', fontsize=16)
+        ax[1].set_xlabel('Epochs', color='grey', fontsize=12)
+        ax[1].set_ylabel('Loss', color='grey', fontsize=12)
+        ax[1].grid(True)
+
+        plt.show()
+
+    # Evaluate & Plot
+    score_test = k_model.evaluate(X_test_scaled, y_test, verbose=0)
+    test_loss = score_test[0]
+    test_accuracy = score_test[1]
+
+    # Print evaluation results on test dataset
+    print("model_id_{}: lr={}, batch_size={}, epochs={}".format(model_id, lr, batch_size, epochs))
+    print('Test loss:', test_loss)
+    print('Test accuracy:', test_accuracy)
+
+    # Zapisanie do dictionary
+    models["{}".format(model_id)] = {
+        "lr": lr,
+        "batch_size": batch_size,
+        "epochs": epochs,
+        "test_loss": test_loss,
+        "test_accuracy": test_accuracy
+    }
+
+    # insert statement
+    sql_statement = """
+                insert into dl_models (python_model_id, lr, batch_size, epochs, test_loss, test_accuracy) values ({}, {}, {}, {}, {}, {})
+            """.format(model_id, lr, batch_size, epochs, test_loss, test_accuracy)
+
+    try:
+        cur.execute(sql_statement)
+        conn.commit()
+
+    except Exception:
+        print("Cannot save model parameters to the database")
+        print(str(Exception))
+
+        print("Trying to re-establish database connection...")
+        try:
+            cur.close()
+            conn.close()
+        except:
+            print("Cannot close current connection")
+
+        # Wait 5s and try re-establish connection
+        sleep(5)
+
+        try:
+            conn = psycopg2.connect(host='192.168.0.101', user='bartek', password='Aga', database='logs', port=5432)
+            conn.set_client_encoding('UTF8')
+            cur = conn.cursor()
+            print("New connection has been established")
+        except:
+            print("I am unable to connect to the database.")
+
+            sleep(180)
+            try:
+                conn = psycopg2.connect(host='192.168.0.101', user='bartek', password='Aga', database='logs', port=5432)
+                conn.set_client_encoding('UTF8')
+                cur = conn.cursor()
+                print("New connection has been established")
+            except:
+                print("I am unable to connect to the database, quitting...")
+                sys.exit()
+
+        cur.execute(sql_statement)
+        conn.commit()
+
+    # Best model?
+    current_best = models.get("best", None)
+    if current_best is not None and current_best["test_accuracy"] >= test_accuracy:
+        pass
+
+    else:
+        # aktualizacja best
+        models["best"] = {
+            "model_id": model_id,
+            "lr": lr,
+            "batch_size": batch_size,
+            "epochs": epochs,
+            "test_loss": test_loss,
+            "test_accuracy": test_accuracy
+        }
+        # nowy wykres z historią uczenia
+        plot_accuracy_and_loss(history, test_accuracy)
+
+
+# %%
+models = {}
+
+# %%
+
+# lrs = [10, 1, 0.1, 0.01, 0.001, 0.0001]
+lrs = [10]
+epochss = [6000]
+batch_sizes = [None, 16, 32, 64, 128, 256, 516, 1024, 2048, 4096]
+patiences = [2, 7, 12, 20]
+
+model_id = 1730
+
+kombinacje = len(lrs) * len(epochss) * len(batch_sizes) * len(patiences) + model_id
+
+for lr in lrs:
+    for epochs in epochss:
+        for batch_size in batch_sizes:
+            for patience in patiences:
+                model_id += 1
+                print("\n\nFitting model {} / {}".format(model_id, kombinacje))
+                fit_and_evaluate_model(models, model_id, lr=lr, batch_size=batch_size,
+                                       epochs=epochs, patience=patience)
+
+        print("\n\nBest model:")
+        print(models["best"])
+
