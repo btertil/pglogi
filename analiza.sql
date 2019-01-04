2b451ad2ba25b11903fb2fcce3c7562d04c9ef81
Bartosz Tertil <bartosz.tertil@gmail.com>
2019-01-04 22:52:27 +0100

    basic reporting + oddzielenie run_id >= 700 dla win gpu dla modeli w jupyter lab na win (w analiza.sql)

--- analiza.sql
+++ analiza.sql
@@ -7,7 +7,8 @@ select * from dl_models order by test_accuracy desc;
 select batch_size, count(*) ile, avg(test_accuracy) from dl_models group by batch_size order by avg(test_accuracy) desc;
 select lr, count(*) ile, avg(test_accuracy) from dl_models where lr <> 0.1 group by lr order by avg(test_accuracy) desc;
 
-select epochs, count(*) ile, avg(test_accuracy) from dl_models group by epochs order by avg(test_accuracy) desc;
+select epochs, count(*) ile, avg(test_accuracy) from dl_models group by 1 order by avg(test_accuracy) desc;
+select run_id, count(*) ile, avg(test_accuracy) from v_dl_models_runs group by 1 order by avg(test_accuracy) desc;
 
 -- to warto tylko jeśłi inne hyperparameters, beta, beta2 itd
 select
@@ -39,7 +40,8 @@ create or replace view v_dl_models_runs as
             when python_model_id >= 250 and python_model_id < 310 then 4
             when python_model_id >= 250 and python_model_id < 460 then 5
             when python_model_id >= 460 and python_model_id < 668 then 6
-            when python_model_id >= 668 and python_model_id < 1494 then 7
+            when python_model_id >= 668 and python_model_id < 818 then 7
+            when python_model_id >= 818 and python_model_id < 1494 then 700
             when python_model_id >= 1494 and python_model_id < 1731 then 8
             when python_model_id >= 1731 and python_model_id < 1886 then 9
             when python_model_id >= 1886 and python_model_id < 1948 then 10
@@ -54,6 +56,7 @@ create or replace view v_dl_models_runs as
                 when python_model_id >= 250 and python_model_id < 460 then 5
                 when python_model_id >= 460 and python_model_id < 668 then 6
                 when python_model_id >= 668 and python_model_id < 1494 then 7
+                when python_model_id >= 818 and python_model_id < 1494 then 700
                 when python_model_id >= 1494 and python_model_id < 1731 then 8
                 when python_model_id >= 1731 and python_model_id < 1886 then 9
                 when python_model_id >= 1886 and python_model_id < 1948 then 10
@@ -72,6 +75,7 @@ create or replace view v_dl_models_runs as
             when python_model_id >= 250 and python_model_id < 460 then 5
             when python_model_id >= 460 and python_model_id < 668 then 6
             when python_model_id >= 668 and python_model_id < 1494 then 7
+            when python_model_id >= 818 and python_model_id < 1494 then 700
             when python_model_id >= 1494 and python_model_id < 1731 then 8
             when python_model_id >= 1731 and python_model_id < 1886 then 9
             when python_model_id >= 1886 and python_model_id < 1948 then 10
@@ -83,7 +87,12 @@ select * from v_dl_models_runs;
 
 -- view v_dl_models_performance
 create or replace view v_dl_models_performance as
-  select * from v_dl_models_runs order by test_accuracy desc;
+  select
+    r.*,
+    rank() over (order by test_accuracy desc) model_rank
+from v_dl_models_runs r
+order by
+    test_accuracy desc;
 
 select * from v_dl_models_performance limit 25;
 
@@ -107,28 +116,33 @@ order by 5 desc;
 
 
 -- best model per run_id
-select
-   id,
-   python_model_id,
-   lr,
-   batch_size,
-   epochs,
-   test_loss,
-   test_accuracy,
-   entered,
-   run_id,
-   time_diff training_time,
-   patience
-from (
+drop view if exists v_dl_models_best_per_run;
+create or replace view v_dl_models_best_per_run as
     select
-      r.*,
-      max(test_accuracy) over (partition by run_id) ma
-    from v_dl_models_runs r
-    ) s
-where
-  ma = test_accuracy
-order by
-  test_accuracy desc;
+       id,
+       python_model_id,
+       lr,
+       batch_size,
+       epochs,
+       test_loss,
+       test_accuracy,
+       entered,
+       run_id,
+       time_diff training_time,
+       model_rank,
+       patience
+    from (
+        select
+          r.*,
+          max(test_accuracy) over (partition by run_id) ma
+        from v_dl_models_performance r
+        ) s
+    where
+      ma = test_accuracy
+    order by
+      test_accuracy desc;
+
+select * from v_dl_models_best_per_run;
 
 -- updates
 
@@ -140,3 +154,21 @@ select '2 days 2 seconds' :: interval interval_time, extract(epoch from '2 days 
 
 select count(*) ile, max(id) max_id, max(test_accuracy) best from dl_models;
 
+
+
+-- reporting
+-- ----------
+
+-- basic selects
+
+select * from v_dl_models_runs where run_id >= 7 and patience is null and id > 1016;
+select * from v_dl_models_performance limit 50;
+
+-- best models per run
+select * from v_dl_models_best_per_run;
+
+
+-- current progress
+select count(*) ile, max(id) max_id, max(test_accuracy) best from dl_models;
+
+commit;
